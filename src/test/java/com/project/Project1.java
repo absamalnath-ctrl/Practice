a3ec016816ec71081812199773c93beee125b9b7
absamalnath-ctrl <abs.amalnath@gmail.com>
2025-12-23 13:20:40 +0530

    Reedited

--- src/test/java/com/project/Project1.java
+++ src/test/java/com/project/Project1.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Project1 {
 	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Welcome Balaji");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://omrbranch.com/");
 		driver.manage().window().maximize();
